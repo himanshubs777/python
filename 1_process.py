32c75c264837336625056654a8ff0e0327b91115
himanshu b sarvaiya <himanshusarvaiya77@gmail.com>
2026-01-03 13:57:13 +0530

    first commit

---
+++ 1_process.py
@@ -0,0 +1,9 @@
+# write a program to calculate the area  a rectangle
+
+length = int(input("Enter the length :"))
+width = int(input("Enter the width :"))
+
+area = length * width
+
+# print ("Area of rectangle is :", area)
+print(f"Area of rectangle is : {area}")
---
+++ task1.py
@@ -0,0 +1,26 @@
+# ask
+# write a program to create variables to store and display below person detail
+# name
+# address
+# city
+# pincode
+# age
+# weight
+# gender
+
+name = 'Rama kaka'
+address = '123,ubhi society,kala nala'
+city = ''
+pincode = '302001'
+age = 25
+weight = 70
+gender = 'male'
+print("Name:", name)
+print("Address:", address)  
+print("City:", city)
+print("Pincode:", pincode)
+print("Age:", age)
+print("Weight:", weight)
+print("Gender:", gender)
+# endask
+
